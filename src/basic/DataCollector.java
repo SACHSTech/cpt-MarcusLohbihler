865d3d19603a645a1b30b64057aef177cc9ad219
Marcus_Lohbihler <Marcus.Lohbihler23@ycdsbk12.ca>
2023-01-18 09:18:51 -0500

    changed file path, added try/catch to filereader

--- src/basic/DataCollector.java
+++ src/basic/DataCollector.java
@@ -1,14 +1,21 @@
 package basic;
 import java.util.ArrayList;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class DataCollector {
     private ArrayList<Graphdata> dataList; 
-    public DataCollector (ArrayList<Graphdata> theDataList){
+    public DataCollector (ArrayList<Graphdata> theDataList)throws FileNotFoundException{
         dataList = new ArrayList<Graphdata>();
-        String fileName = "C:\Users\marcu\Downloads\cumulative-number-of-objects-launched-into-outer-space.xlsx";
-        FileReader input = new FileReader(fileName);
+        String fileName = "Resources/cumulative-number-of-objects-launched-into-outer-space - cumulative-number-of-objects-launched-into-outer-space.csv";
+        try (FileReader input = new FileReader(fileName)){
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
     
 
