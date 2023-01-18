0a384b32f5499a36b9679aacb2bc97974db03faf
Marcus_Lohbihler <Marcus.Lohbihler23@ycdsbk12.ca>
2023-01-18 09:16:29 -0500

    updated csv path

--- src/basic/DataCollector.java
+++ src/basic/DataCollector.java
@@ -1,5 +1,15 @@
 package basic;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class DataCollector {
+    private ArrayList<Graphdata> dataList; 
+    public DataCollector (ArrayList<Graphdata> theDataList){
+        dataList = new ArrayList<Graphdata>();
+        String fileName = "C:\Users\marcu\Downloads\cumulative-number-of-objects-launched-into-outer-space.xlsx";
+        FileReader input = new FileReader(fileName);
+    }
     
+
 }
