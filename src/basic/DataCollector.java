efb46fe92c3adac17775115852a3ed32af74f3b0
Marcus_Lohbihler <Marcus.Lohbihler23@ycdsbk12.ca>
2023-01-18 09:42:21 -0500

    Trying to read file

--- src/basic/DataCollector.java
+++ src/basic/DataCollector.java
@@ -9,9 +9,9 @@ public class DataCollector {
     private ArrayList<Graphdata> dataList; 
     public DataCollector (ArrayList<Graphdata> theDataList)throws FileNotFoundException{
         dataList = new ArrayList<Graphdata>();
-        String fileName = "Resources/cumulative-number-of-objects-launched-into-outer-space - cumulative-number-of-objects-launched-into-outer-space.csv";
+        String fileName = "c:/Users/marcu/cpt-MarcusLohbihler/Resources/cumulative-number-of-objects-launched-into-outer-space - cumulative-number-of-objects-launched-into-outer-space.csv";
         try (FileReader input = new FileReader(fileName)){
-
+            
         }
         catch(IOException e){
             e.printStackTrace();
